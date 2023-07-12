a6e4aa7da9cf4d105bafc2dfc05a1bced382d0a7
Esteban <esteban.salazar@utp.edu.co>
2023-07-12 14:43:29 -0500

    added metric functions

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sneiaTools',
+    version = '0.0.1',
+    license = 'MIT',
+    description = 'A python package that will include all the tools that the SNEIA research team will develop and need to carry out their tasks.',
+    author = 'SNEIA Research Team',
+    author_email = 'sneia@utp.edu.co',
+    install_requires = ['numpy'],
+    url = 'https://github.com/Semillero-de-neuroinformatica/SNEIA-tools',
+    packages = find_packages()
+)
